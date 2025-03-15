922f9080abfa679793a2b7b2761fdac21cb966c2
johnson <johnsonraja1110@gmail.com>
2025-03-15 12:31:07 +0530

    second-version

---
+++ collection.js
@@ -0,0 +1,18 @@
+var productContainer = document.getElementById("products")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function() {
+   var enteredValue = event.target.value.toUpperCase()
+
+   for(i=0;i<productlist.length; i++) {
+    var productname = productlist[i].querySelector("p").textContent
+
+    if(productname.toUpperCase().indexOf(enteredValue)<0){
+        productlist[i].style.display ="none"
+    }
+    else {
+        productlist[i].style.display = "block"
+    }
+   }
+})
